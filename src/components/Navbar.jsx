4cc103228c26b0210bdc98f1e102829710d873bd
tu le <khach2010@gmail.com>
2021-11-18 06:29:49 +0100

    basic template setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import {Button, Menu, Typography, Avatar} from 'antd'
-import {Link} from 'react-router-dom'
-import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
+import React, { useState, useEffect } from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import icon from '../images/bitcoin.png'
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar Component</h1>
+    <div className='nav-container' >
+      <div className="logo-container">
+        <Avatar src={icon} size='large' />
+        <Typography.Title level={2} className="logo"><Link to="/">Crypto News</Link></Typography.Title>
+      </div>
+      <Menu theme='dark' >
+          <Menu.Item icon={<HomeOutlined/>}>
+            <Link to='/' >Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined/>}>
+            <Link to='/cryptocurrencies' >Crytocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined/>}>
+            <Link to='/exchanges' >Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined/>}>
+            <Link to='/news' >News</Link>
+          </Menu.Item>
+        </Menu>
     </div>
   )
 }
